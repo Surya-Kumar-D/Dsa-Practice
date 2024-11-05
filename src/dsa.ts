705ff6e9d6afac65b4143d4ec5f9b2b432a4260b
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-05 15:24:01 +0530

    can place flower leetcode challenge

--- src/dsa.ts
+++ src/dsa.ts
@@ -334,3 +334,22 @@
 // }
 
 // console.log(kidsWithCandies([2, 3, 5, 1, 3], 3));
+
+//605. Can Place Flowers
+// function canPlaceFlowers(flowerbed: number[], n: number): boolean {
+//   for (let i = 0; i < flowerbed.length; i++) {
+//     if (
+//       flowerbed[i] === 0 &&
+//       (i === 0 || flowerbed[i - 1] === 0) &&
+//       (i === flowerbed.length - 1 || flowerbed[i + 1] === 0)
+//     ) {
+//       flowerbed[i] = 1;
+//       n--;
+//       if (n === 0) return true;
+//     }
+//   }
+//   return n <= 0;
+// }
+
+// console.log(canPlaceFlowers([1, 0, 0, 0, 1], 1));
+// console.log(canPlaceFlowers([0, 0, 1, 0, 1], 1));
