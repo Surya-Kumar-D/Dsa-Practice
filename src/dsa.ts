1b0a3a1481219abd8e6d52dad0b9772e12df0825
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-11-08 17:01:24 +0530

    Increasing Triplet Subsequence

--- src/dsa.ts
+++ src/dsa.ts
@@ -385,18 +385,43 @@
 
 //238. Product of Array Except Self
 
-function productExceptSelf(nums: number[]): number[] {
-  let res = [1];
-  for (let i = 1; i < nums.length; i++) {
-    res.push(res[i - 1] * nums[i - 1]);
+// function productExceptSelf(nums: number[]): number[] {
+//   let res = [1];
+//   for (let i = 1; i < nums.length; i++) {
+//     res.push(res[i - 1] * nums[i - 1]);
+//   }
+//   console.log(res);
+//   let suffixProduct = 1;
+//   for (let i = nums.length - 2; i >= 0; i--) {
+//     suffixProduct *= nums[i + 1];
+//     res[i] = res[i] * suffixProduct;
+//   }
+//   return res;
+// }
+
+// console.log(productExceptSelf([1, 2, 3, 4]));
+
+//334. Increasing Triplet Subsequence
+
+function increasingTriplet(nums: number[]): boolean {
+  let first = Infinity;
+  let second = Infinity;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] <= first) {
+      first = nums[i];
+      console.log(nums[i]);
+    } else if (nums[i] <= second) {
+      second = nums[i];
+      console.log(nums[i]);
+    } else {
+      console.log(nums[i]);
+      return true;
+    }
   }
-  console.log(res);
-  let suffixProduct = 1;
-  for (let i = nums.length - 2; i >= 0; i--) {
-    suffixProduct *= nums[i + 1];
-    res[i] = res[i] * suffixProduct;
-  }
-  return res;
+  return false;
 }
 
-console.log(productExceptSelf([1, 2, 3, 4]));
+console.log(increasingTriplet([5, 4, 3, 2, 1]));
+console.log(increasingTriplet([1, 2, 3, 4, 5]));
+console.log(increasingTriplet([2, 1, 5, 0, 4, 6]));
+console.log(increasingTriplet([20, 100, 10, 12, 5, 13]));
